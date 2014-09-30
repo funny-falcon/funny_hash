c2cf2f10a6263ed1fc84d72eaaba930a738cc8cd
Sokolov Yura aka funny_falcon <funny.falcon@gmail.com>
2014-09-30 13:25:31 +0400

    classic api

--- funny_hash.h
+++ funny_hash.h
@@ -10,31 +10,23 @@
 
 /* API */
 
-/* 64bit state for 32bit result */
-typedef struct fh_u64_s {
-	uint32_t a, b;
-} fh_u64_t;
 /* mix 32bit integer into 64bit state */
-static inline fh_u64_t fh32_permute(fh_u64_t h, uint32_t v);
+static inline void fh32_permute(uint32_t v, uint32_t *a, uint32_t *b);
 /* hash string, seed with and produce 64bit state */
-static inline fh_u64_t fh32_permute_string(fh_u64_t h, const uint8_t *v, size_t len);
+static inline void fh32_permute_string(const uint8_t *v, size_t len, uint32_t *a, uint32_t *b);
 /* state finalization to 32bit value */
-static inline uint32_t fh32_finalize(fh_u64_t h);
+static inline uint32_t fh32_finalize(uint32_t a, uint32_t b);
 /* convinient function to hash buffer with 32bit seed */
 static inline uint32_t fh32_string_hash(const void *buf, size_t len, uint32_t seed);
 /* convinient function to hash buffer with 64bit seed */
 static inline uint32_t fh32_string_hash2(const void *buf, size_t len, uint32_t seed1, uint32_t seed2);
 
-/* 128bit state for 64bit result */
-typedef struct fh_u128_s {
-	uint64_t a, b;
-} fh_u128_t;
 /* mix 64bit integer into 128bit state */
-static inline fh_u128_t fh64_permute(fh_u128_t h, uint64_t v);
+static inline void fh64_permute(uint64_t v, uint64_t *a, uint64_t *b);
 /* hash string, seed with and produce 128bit state */
-static inline fh_u128_t fh64_permute_string(fh_u128_t h, const uint8_t *v, size_t len);
+static inline void fh64_permute_string(const uint8_t *v, size_t len, uint64_t *a, uint64_t *b);
 /* state finalization to 64bit value */
-static inline uint64_t  fh64_finalize(fh_u128_t h);
+static inline uint64_t  fh64_finalize(uint64_t a, uint64_t b);
 /* convinient function to hash buffer with 64bit seed */
 static inline uint64_t  fh64_string_hash(const void *buf, size_t len, uint64_t seed);
 /* convinient function to hash buffer with 128bit seed */
@@ -56,20 +48,12 @@ static const uint32_t FH_C2 = 0x52c6a5d9;
  *     on x86_64 it tries to pack fh_u64_t in one 64bit register and work on it.
  * gcc does no such mistake */
 static inline void
-fh32_permute_imp(uint32_t *a, uint32_t *b, uint32_t v)
+fh32_permute(uint32_t v, uint32_t *a, uint32_t *b)
 {
 	*a ^= v;
 	*b = FH_ROTL(*b, 16);
 	*a = FH_ROTL(*a, 16) * FH_C1;
 	*b = (*b ^ v) * FH_C2;
-}
-
-static inline fh_u64_t
-fh32_permute(fh_u64_t h, uint32_t v)
-{
-	h.a = FH_ROTL(h.a ^ v, 16) * FH_C1;
-	h.b = (FH_ROTL(h.b, 16) ^ v) * FH_C2;
-	return h;
 }
 
 static inline uint32_t
@@ -87,74 +71,62 @@ fh_load_u32(const uint8_t *v, unsigned len)
 	return x;
 }
 
-static inline fh_u64_t
-fh32_permute_string(fh_u64_t h, const uint8_t *v, size_t len)
+static inline void
+fh32_permute_string(const uint8_t *v, size_t len, uint32_t *a, uint32_t *b)
 {
 	uint32_t t;
 	size_t n = len / 4;
 	for(; n; n--, v+=4) {
 #if FH_READ_UNALIGNED
-		/* clang did something really strange on x86_64 if it were written as
-		h = fh32_permute(h, *(uint32_t*)v);
-		*/
-		fh32_permute_imp(&h.a, &h.b, *(uint32_t*)v);
+		fh32_permute(*(uint32_t*)v, a, b);
 #else
-		fh32_permute_imp(&h.a, &h.b, fh_load_u32(v, 4));
+		fh32_permute(fh_load_u32(v, 4), a, b);
 #endif
 	}
 	t = (uint32_t)len << 24;
 	t |= fh_load_u32(v, len & 3);
-	h.a--;
-	fh32_permute_imp(&h.a, &h.b, t);
-	return h;
+	(*a)--;
+	fh32_permute(t, a, b);
 }
 
 
 static inline uint32_t
-fh32_finalize(fh_u64_t h)
+fh32_finalize(uint32_t a, uint32_t b)
 {
-	fh_u64_t t;
-	t.a = h.a ^ ((h.a >> 16) + (h.b >> 19));
-	t.b = h.b + ((h.a >> 19) ^ (h.b >> 16));
-	t.a *= FH_C1;
-	t.b *= FH_C2;
-	return t.a ^ t.b ^ (t.a >> 16) ^ (t.b >> 17);
+	uint32_t c, d;
+	c = a ^ ((a >> 16) + (b >> 19));
+	d = b + ((a >> 19) ^ (b >> 16));
+	c *= FH_C1;
+	d *= FH_C2;
+	return c ^ d ^ (c >> 16) ^ (d >> 17);
 }
 
 static inline uint32_t
 fh32_string_hash(const void* d, size_t len, uint32_t seed)
 {
-	fh_u64_t v = {seed, seed};
-	v = fh32_permute_string(v, (const uint8_t*)d, len);
-	return fh32_finalize(v);
+	uint32_t a = seed, b = seed;
+	fh32_permute_string((const uint8_t*)d, len, &a, &b);
+	return fh32_finalize(a, b);
 }
 
 static inline uint32_t
 fh32_string_hash2(const void* d, size_t len, uint32_t seed1, uint32_t seed2)
 {
-	fh_u64_t v = {seed1, seed2};
-	v = fh32_permute_string(v, (const uint8_t*)d, len);
-	return fh32_finalize(v);
+	uint32_t a = seed1, b = seed2;
+	fh32_permute_string((const uint8_t*)d, len, &a, &b);
+	return fh32_finalize(a, b);
 }
 
 static const uint64_t FH_BC1 = U64_CONSTANT(0xacd5ad43274593b9);
 static const uint64_t FH_BC2 = U64_CONSTANT(0x6956abd6ed558e3d);
 /* it looks like clang is not as good at instruction reordering as gcc */
 static inline void
-fh64_permute_imp(uint64_t *a, uint64_t *b, uint64_t v)
+fh64_permute(uint64_t v, uint64_t *a, uint64_t *b)
 {
 	*a ^= v;
 	*b = FH_ROTL(*b, 32);
 	*a = FH_ROTL(*a, 32) * FH_BC1;
 	*b = (*b ^ v) * FH_BC2;
-}
-
-static inline fh_u128_t
-fh64_permute(fh_u128_t h, uint64_t v)
-{
-	h.a = FH_ROTL(h.a ^ v, 32) * FH_BC1;
-	h.b = (FH_ROTL(h.b, 32) ^ v) * FH_BC2;
-	return h;
 }
 
 static inline uint64_t
@@ -177,53 +149,49 @@ fh_load_u64(const uint8_t *v, unsigned len)
 	return x;
 }
 
-static inline fh_u128_t
-fh64_permute_string(fh_u128_t h, const uint8_t *v, size_t len)
+static inline void
+fh64_permute_string(const uint8_t *v, size_t len, uint64_t *a, uint64_t *b)
 {
 	uint64_t t;
 	size_t n = len / 8;
 	for(; n; n--, v+=8) {
 #if FH_READ_UNALIGNED
-		/* it should be written like this, but clang faster with explicit ordering
-		h = fh64_permute(h, *(uint64_t*)v);
-		*/
-		fh64_permute_imp(&h.a, &h.b, *(uint64_t*)v);
+		fh64_permute(*(uint64_t*)v, a, b);
 #else
-		fh64_permute_imp(&h.a, &h.b, fh_load_u64(v, 8));
+		fh64_permute(fh_load_u64(v, 8), a, b);
 #endif
 	}
 	t = (uint64_t)len << 56;
 	t |= fh_load_u64(v, len & 7);
-	h.a--;
-	fh64_permute_imp(&h.a, &h.b, t);
-	return h;
+	(*a)--;
+	fh64_permute(t, a, b);
 }
 
 static inline uint64_t
-fh64_finalize(fh_u128_t h)
+fh64_finalize(uint64_t a, uint64_t b)
 {
-	fh_u128_t t;
-	t.a = h.a ^ ((h.a >> 32) + (h.b >> 35));
-	t.b = h.b + ((h.a >> 35) ^ (h.b >> 32));
-	t.a *= FH_BC1;
-	t.b *= FH_BC2;
-	return t.a ^ t.b ^ (t.a >> 32) ^ (t.b >> 33);
+	uint64_t c, d;
+	c = a ^ ((a >> 32) + (b >> 35));
+	d = b + ((a >> 35) ^ (b >> 32));
+	c *= FH_BC1;
+	d *= FH_BC2;
+	return c ^ d ^ (c >> 32) ^ (d >> 33);
 }
 
 static inline uint64_t
 fh64_string_hash(const void* d, size_t len, uint64_t seed)
 {
-	fh_u128_t v = {seed, seed};
-	v = fh64_permute_string(v, (const uint8_t*)d, len);
-	return fh64_finalize(v);
+	uint64_t a = seed, b = seed;
+	fh64_permute_string((const uint8_t*)d, len, &a, &b);
+	return fh64_finalize(a, b);
 }
 
 static inline uint64_t
 fh64_string_hash2(const void* d, size_t len, uint64_t seed1, uint64_t seed2)
 {
-	fh_u128_t v = {seed1, seed2};
-	v = fh64_permute_string(v, (const uint8_t*)d, len);
-	return fh64_finalize(v);
+	uint64_t a = seed1, b = seed2;
+	fh64_permute_string((const uint8_t*)d, len, &a, &b);
+	return fh64_finalize(a, b);
 }
 
 #undef FH_ROTL
